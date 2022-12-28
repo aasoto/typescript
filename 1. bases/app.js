ad4a33d3bab5e40c19bf8c1583dcfa3ab099c068
aasoto <andresalfredosotosuarez@gmail.com>
2022-12-28 18:34:23 -0500

    inicio de curso

---
+++ 1. bases/app.js
@@ -0,0 +1,9 @@
+"use strict";
+const msg = "Hola mundo";
+const city = {
+    name: 'Valledupar',
+    country: 'Colombia',
+    population: 450000
+};
+city.population = 500000;
+console.log(city.population + 1);
---
+++ 1. bases/app.ts
@@ -0,0 +1,11 @@
+const msg: string = "Hola mundo"
+
+const city = {
+  name: 'Valledupar',
+  country: 'Colombia',
+  population: 450000
+}
+
+city.population = 500000
+
+console.log(city.population + 1)
