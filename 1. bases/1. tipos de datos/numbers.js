41c748208d1175052a4add296bc8fe14525099df
aasoto <andresalfredosotosuarez@gmail.com>
2022-12-28 21:59:29 -0500

    inicio tipos de datos

---
+++ 1. bases/1. tipos de datos/numbers.js
@@ -0,0 +1,12 @@
+"use strict";
+(() => {
+    const avengers = 10;
+    const villians = 20;
+    if (avengers < villians) {
+        console.log('Estamos en problemas');
+    }
+    else {
+        console.log('Nos salvamos');
+    }
+    console.log({ avengers });
+})();
