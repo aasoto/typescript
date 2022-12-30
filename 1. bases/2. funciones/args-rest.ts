b86452bc25eac6cb2cf8e76d380e12f4e3f28c54
aasoto <andresalfredosotosuarez@gmail.com>
2022-12-29 21:49:17 -0500

    función rest

---
+++ 1. bases/2. funciones/args-rest.ts
@@ -0,0 +1,16 @@
+(()=>{
+  /**
+   * Una función con argumentos rest es aquella donde no se sabe cuantos o que número de argumentos se van a pasar
+   */
+  const fullName = ( firstName: string, ...restArgs:string[] ):string => {
+    /**
+     * el resto de argumentos se colocan de esta manera debido a que  el tipo string[]
+     * es un arreglo y lo concatena por medio de join(' ')
+     */
+    return `${firstName} ${restArgs.join(' ')}`;
+  }
+
+  console.log(fullName('Andrés'));
+  console.log(fullName('Andrés', 'Soto'));
+  console.log(fullName('Andrés', 'Soto', 'Suárez'));
+})()
